3c83b95bc9e374c40059333b864a9c0ec7249139
Galang <galanggalang2378@gmail.com>
2024-11-19 02:09:47 +0700

    first init github

---
+++ src/services/getAllProducts.js
@@ -0,0 +1,35 @@
+export const getAllProducts = () => {
+  return [
+    {
+      id: 'ST-1-07-VESPA-001',
+      slug: 'gts-super-tech-300',
+      name: "VESPA GTS SUPER TECH 300",
+      category: 'Vespa Matic',
+      price: 164_500_000,
+      stock: 25,
+      imageUrl: '/assets/images/GTS Super Tech.png',
+      description: `Top-of-line Vespa GTS model, combining the ultimate sports attitude with cutting-edge technology.`
+    },
+    {
+      id: 'VSS-1-07-VESPA-SS-002',
+      slug: 'vespa-sprint-s-150',
+      name: "VESPA SPRINT S 150",
+      category: 'Vespa Matic',
+      price: 59_600_000,
+      stock: 39,
+      imageUrl: `/assets/images/Sprint S.png`,
+      description: `The Vespa Sprint S adds a series of novelties to the already-sporty Vespa Sprint. Accentuates further Vespa Sprint's sporty styling and is designed to be ridden by those that have the most competitive attitude among Vespa lovers.`
+    },
+    {
+      id: 'VPS-1-07-VESPA-003',
+      slug: 'vespa-primavera-s-150',
+      name: "VESPA PRIMAVERA S 150",
+      category: 'Vespa Matic',
+      price: 56_600_000,
+      stock: 44,
+      imageUrl: `/assets/images/Primavera S.png`,
+      description: `Vespa Primavera S is the bold, stylish and sporty version of the classic Primavera. This model came to life for the trend setters, the audacious who want to show their true selves in the most distinguished way from others, to be the one and define their own style to stand out from the crowd.`
+    },
+
+  ]
+}
